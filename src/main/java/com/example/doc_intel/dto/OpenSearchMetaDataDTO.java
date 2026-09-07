607e11976dd0f91ce1a2a7adb6ba3c33fddd64bd
Somesh Sunariwal <someshsunariwal@gmail.com>
2026-09-07 21:28:15 +0530

    Code Fix

--- src/main/java/com/example/doc_intel/dto/OpenSearchMetaDataDTO.java
+++ src/main/java/com/example/doc_intel/dto/OpenSearchMetaDataDTO.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OpenSearchMetaDataDTO {
-
     private String fileName;
     private Integer pageNumber;
     private Integer lineNumber;
--- src/main/java/com/example/doc_intel/dto/QuestionRequestDTO.java
+++ src/main/java/com/example/doc_intel/dto/QuestionRequestDTO.java
@@ -8,5 +8,4 @@ import lombok.Data;
 @AllArgsConstructor
 public class QuestionRequestDTO {
     String question;
-
 }
