288dd4c0a32f3d816e666c929a9bd9b3cf73a5f2
Somesh Sunariwal <someshsunariwal@gmail.com>
2026-09-09 13:13:46 +0530

    Updated Repository Entity.

--- src/main/java/com/example/doc_intel/Repository/UserRepository.java
+++ src/main/java/com/example/doc_intel/Repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.doc_intel.Repository;
 
-import com.example.doc_intel.Entity.DocumentEntity;
+import com.example.doc_intel.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 @Component
-public interface UserRepository extends JpaRepository<DocumentEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 }
